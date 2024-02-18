42563c8bf4ae4315a9e34d5ae8aa92b3a22b00c3
CeraStroh <cera.stroh@outlook.com>
2024-02-18 09:22:00 -0600

    delete unused data

--- study-application/app/lib/placeholder-data.js
+++ study-application/app/lib/placeholder-data.js
@@ -33,51 +33,6 @@ const studysets = [
 	},
 ];
 
-const customers = [
-  {
-    id: '3958dc9e-712f-4377-85e9-fec4b6a6442b',
-    name: 'Delba de Oliveira',
-    email: 'delba@oliveira.com',
-    image_url: '/customers/delba-de-oliveira.png',
-  },
-  {
-    id: '3958dc9e-742f-4377-85e9-fec4b6a6442b',
-    name: 'Lee Robinson',
-    email: 'lee@robinson.com',
-    image_url: '/customers/lee-robinson.png',
-  },
-];
-
-const invoices = [
-	{
-		customer_id: customers[0].id,
-		amount: 15795,
-		status: 'pending',
-		date: '2022-12-06',
-	},
-	{
-		customer_id: customers[1].id,
-		amount: 20348,
-		status: 'pending',
-		date: '2022-11-14',
-	},
-];
-
-const revenue = [
-	{ month: 'Jan', revenue: 2000 },
-	{ month: 'Feb', revenue: 1800 },
-	{ month: 'Mar', revenue: 2200 },
-	{ month: 'Apr', revenue: 2500 },
-	{ month: 'May', revenue: 2300 },
-	{ month: 'Jun', revenue: 3200 },
-	{ month: 'Jul', revenue: 3500 },
-	{ month: 'Aug', revenue: 3700 },
-	{ month: 'Sep', revenue: 2500 },
-	{ month: 'Oct', revenue: 2800 },
-	{ month: 'Nov', revenue: 3000 },
-	{ month: 'Dec', revenue: 4800 },
-];
-
 module.exports = {
 	users,
 	studysets,
