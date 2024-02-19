02a60e46c118b88bd03cbaaa6ac69b150c4dd7f8
CeraStroh <cera.stroh@outlook.com>
2024-02-19 17:51:25 -0600

    Create definitions.ts

---
+++ app/lib/definitions.ts
@@ -0,0 +1,22 @@
+// This file contains type definitions for your data.
+// It describes the shape of the data, and what data type each property should accept.
+export type User = {
+    user_id: string;
+    name: string;
+    email: string;
+    password: string;
+  };
+  
+export type StudySets = {
+    user_id: string;
+    set_id: string;
+    title: string;
+    pairs: number;
+    date: string;
+};
+  
+export type StudySetForm = {
+    set_id: string;
+    term: string;
+    definition: string;
+};
