7bb028d00fda543b381c1a3110d63f13d5e36805
CeraStroh <cera.stroh@outlook.com>
2024-02-19 17:50:48 -0600

    add Create Study Set button

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <p>Home Page</p>;
+import { CreateStudySet } from '@/app/ui/study-set/buttons';
+
+export default async function Page() {
+  return (
+    <main>
+      <div className="w-full">
+        <div className="flex w-full items-center justify-between">
+          <h1>Home Page</h1>
+        </div>
+        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+          <CreateStudySet />
+        </div>
+      </div>
+    </main>
+  );
 }
