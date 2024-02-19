05f898b9eddc82a2bb885f3dc20e6e217c483080
CeraStroh <cera.stroh@outlook.com>
2024-02-19 17:51:52 -0600

    update placeholder study sets

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -13,29 +13,123 @@ const studysets = [
 	{
 		user_id: users[0].user_id,
 		set_id: '65473',
-		title: 'US Capitals',
-		pairs: '50',
+		title: 'Midwest US Capitals',
+		pairs: 12,
 		date: '2024-2-18',
 	},
 	{
 		user_id: users[0].user_id,
 		set_id: '53476',
 		title: 'COSC Classes',
-		pairs: '4',
+		pairs: 5,
 		date: '2024-2-17',
 	},
 	{
 		user_id: users[0].user_id,
 		set_id: '45364',
-		title: 'Cybersecurity Terms',
-		pairs: '5',
+		title: 'Financial Accounting Exam 1',
+		pairs: 4,
 		date: '2024-2-16',
 	},
 ];
 
+const MidwestUSCapitals = [
+	{
+		term: 'Springfield',
+		definition: 'Illinois',
+	},
+	{
+		term: 'Indianapolis',
+		definition: 'Indiana',
+	},
+	{
+		term: 'Des Moines',
+		definition: 'Iowa',
+	},
+	{
+		term: 'Topeka',
+		definition: 'Kansas',
+	},
+	{
+		term: 'Lansing',
+		definition: 'Michigan',
+	},
+	{
+		term: 'Saint Paul',
+		definition: 'Minnesota',
+	},
+	{
+		term: 'Jefferson City',
+		definition: 'Missouri',
+	},
+	{
+		term: 'Lincoln',
+		definition: 'Nebraska',
+	},
+	{
+		term: 'Bismarck',
+		definition: 'North Dakota',
+	},
+	{
+		term: 'Columbus',
+		definition: 'Ohio',
+	},
+	{
+		term: 'Pierre',
+		definition: 'South Dakota',
+	},
+	{
+		term: 'Madison',
+		definition: 'Wisconsin',
+	},
+]
+
+const COSCClasses = [
+	{
+		term: 'COSC-109',
+		definition: 'Intro to Information Mgmt',
+	},
+	{
+		term: 'COSC-110',
+		definition: 'Intro to Computer Science',
+	},
+	{
+		term: 'COSC-130',
+		definition: 'Data Structures',
+	},
+	{
+		term: 'COSC-346',
+		definition: 'Cybersecurity',
+	},
+	{
+		term: 'COSC-420',
+		definition: 'Senior Project',
+	},
+]
+
+const FinancialAccountingExam1 = [
+	{
+		term: 'Accounting Equation',
+		definition: 'Assets = Liabilities + Stockholders\' Equity',
+	},
+	{
+		term: 'Assets',
+		definition: 'things company owns (land, cash, equipment, buildings, inventory, supplies, computers, trucks, office furniture)',
+	},
+	{
+		term: 'Liabilities',
+		definition: 'amount business owes/debt (Note Payable-loan, Accounts payable)',
+	},
+	{
+		term: 'Stockholders\' Equity',
+		definition: 'owners\' claim to business',
+	},
+]
+
 module.exports = {
 	users,
 	studysets,
-	// invoices,
-	//revenue,
+	MidwestUSCapitals,
+	COSCClasses,
+	FinancialAccountingExam1,
 };
