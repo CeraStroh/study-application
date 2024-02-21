e3713c586c41d670c8b5aaf6fac3bdd2519c46a0
CeraStroh <cera.stroh@outlook.com>
2024-02-21 17:59:14 -0600

    create study-set/create page

---
+++ app/home/study-set/create/page.tsx
@@ -0,0 +1,11 @@
+import Form from '@/app/ui/study-set/create-form';
+//import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+//import { fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page() {
+  return (
+    <main>
+      <Form />
+    </main>
+  );
+}
