9cb11bec13e2e09c6b985be03798116fb2753471
CeraStroh <cera.stroh@outlook.com>
2024-02-26 18:23:22 -0600

    explore new way to add pairs

--- app/ui/study-set/create-form.tsx
+++ app/ui/study-set/create-form.tsx
@@ -17,15 +17,26 @@ import { useState } from 'react';
 export default function Form() {
   const initialState = { message: null, errors: {} };
   const [state, dispatch] = useFormState(createStudySet, initialState);
-  const [pairs, setPairs] = useState<JSX.Element[]>([]);
-  
+  const [pairs, setPairs] = useState([{ term: "", definition: "" }]);
+
   const handleAddPair = () => {
-    setPairs(prevPairs => [...prevPairs, <NewPair key={pairs.length} onDelete={() => handleDeletePair(prevPairs.length)} />]);
+    setPairs([...pairs, { term: "", definition: "" }]);
+  };
+
+  const handleChange = (event: any, index: number) => {
+    let { name, value } = event.target;
+    console.log(event.target)
+    let onChangeValue = [...pairs];
+    onChangeValue[index][name] = value;
+    console.log(name);
+    setPairs(onChangeValue);
   };
 
   const handleDeletePair = (index: number) => {
-    setPairs(prevPairs => prevPairs.filter((_, i) => i !== index));
-  }
+    const newArray = [...pairs];
+    newArray.splice(index, 1);
+    setPairs(newArray);
+  };
 
   return (
     <form action={dispatch}>
@@ -58,12 +69,73 @@ export default function Form() {
         </div>
 
         {/* Term-Definition pairs */}
-        <div>
-          {/* Render existing pairs */}
-          {pairs.map(pair => pair)}
-
-          {/* Button to add a new component */}
-          <Button onClick={handleAddPair}>Add pair</Button>
+        <div className="mb-1 columns-2">
+          <div className="container">
+            {pairs.map((item, index) => (
+              <div className="input_container" key={index}>
+                <div className="w-full">
+                  <label htmlFor="term" className="mb-2 block text-sm font-medium text-black">
+                    Term
+                  </label>
+                  <div className='relative'>
+                    <input
+                      id="term"
+                      name="term"
+                      type="string"
+                      value={item.term}
+                      onChange={(event) => handleChange(event, index)}
+                      placeholder="Enter term"
+                      className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
+                      aria-describedby="term-error"
+                    />
+                    {/* <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" /> */}
+                  </div>
+                </div>
+                {/* <div id="term-error" aria-live="polite" aria-atomic="true">
+                  {state.errors?.term &&
+                    state.errors.term.map((error: string) => (
+                      <p className="mt-2 text-sm text-red-500" key={error}>
+                        {error}
+                      </p>
+                    ))}
+                </div> */}
+                <div className="w-full">
+                  <label htmlFor="definition" className="mb-2 block text-sm font-medium text-black">
+                    Definition
+                  </label>
+                  <div className="relative">
+                    <input
+                      id="definition"
+                      name="definition"
+                      type="string"
+                      value={item.definition}
+                      onChange={(event) => handleChange(event, index)}
+                      placeholder="Enter definition"
+                      className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
+                      aria-describedby="definition-error"
+                    />
+                    {/* <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" /> */}
+                  </div>
+                </div>
+                {/* <div id="definition-error" aria-live="polite" aria-atomic="true">
+                  {state.errors?.definition &&
+                    state.errors.definition.map((error: string) => (
+                      <p className="mt-2 text-sm text-red-500" key={error}>
+                        {error}
+                      </p>
+                    ))}
+                </div> */}
+                {pairs.length > 1 && (
+                  <Button onClick={() => handleDeletePair(index)}>Delete</Button>
+                )}
+                {index === pairs.length - 1 && (
+                  <Button onClick={() => handleAddPair()}>Add</Button>
+                )}
+              </div>
+            ))}
+            <p>This is what the data looks like</p>
+            <div className="body"> {JSON.stringify(pairs)} </div>
+          </div>
         </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
@@ -78,68 +150,3 @@ export default function Form() {
     </form>
   );
 }
-
-interface NewComponentProps {
-  onDelete: () => void;
-}
-
-const NewPair: React.FC<NewComponentProps> = ({ onDelete }) => {
-
-  return (
-    <div className="mb-1 columns-2">
-      <div className="w-full">
-        <label htmlFor="term" className="mb-2 block text-sm font-medium text-black">
-          Term
-        </label>
-        <div className="relative">
-          <input
-              id="term"
-              name="term"
-              type="string"
-              step="0.01"
-              placeholder="Enter term"
-              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
-              aria-describedby="term-error"
-          />
-          {/* <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" /> */}
-        </div>
-        {/* <div id="term-error" aria-live="polite" aria-atomic="true">
-          {state.errors?.term &&
-            state.errors.term.map((error: string) => (
-              <p className="mt-2 text-sm text-red-500" key={error}>
-                {error}
-              </p>
-            ))}
-        </div> */}
-      </div>
-      <div className="w-full">
-        <label htmlFor="definition" className="mb-2 block text-sm font-medium text-black">
-          Definition
-        </label>
-        <div className="relative">
-          <input
-              id="definition"
-              name="definition"
-              type="string"
-              step="0.01"
-              placeholder="Enter definition"
-              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
-              aria-describedby="definition-error"
-          />
-          {/* <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" /> */}
-        </div>
-        {/* <div id="definition-error" aria-live="polite" aria-atomic="true">
-          {state.errors?.definition &&
-            state.errors.definition.map((error: string) => (
-              <p className="mt-2 text-sm text-red-500" key={error}>
-                {error}
-              </p>
-            ))}
-        </div> */}
-      </div>
-      <div className="w-full">
-        <Button onClick={onDelete}>Delete</Button>
-      </div>
-    </div>
-  );
-};
