9e2f1c07a5fee0980e439cc2d1d57e0d7ea2f661
CeraStroh <cera.stroh@outlook.com>
2024-02-28 14:16:53 -0600

    remove icons, set pair width to full, add spacing

--- app/ui/study-set/create-form.tsx
+++ app/ui/study-set/create-form.tsx
@@ -51,12 +51,10 @@ export default function Form() {
                 id="title"
                 name="title"
                 type="string"
-                step="0.01"
                 placeholder="Enter title"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
                 aria-describedby="title-error"
             />
-            {/* <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" /> */}
           </div>
           <div id="title-error" aria-live="polite" aria-atomic="true">
             {state.errors?.title &&
@@ -69,73 +67,53 @@ export default function Form() {
         </div>
 
         {/* Term-Definition pairs */}
-        <div className="mb-1 columns-2">
-          <div className="container">
-            {pairs.map((item, index) => (
-              <div className="input_container" key={index}>
-                <div className="w-full">
-                  <label htmlFor="term" className="mb-2 block text-sm font-medium text-black">
-                    Term
-                  </label>
-                  <div className='relative'>
-                    <input
-                      id="term"
-                      name="term"
-                      type="string"
-                      value={item.term}
-                      onChange={(event) => handleChange(event, index)}
-                      placeholder="Enter term"
-                      className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
-                      aria-describedby="term-error"
-                    />
-                    {/* <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" /> */}
-                  </div>
-                </div>
-                {/* <div id="term-error" aria-live="polite" aria-atomic="true">
-                  {state.errors?.term &&
-                    state.errors.term.map((error: string) => (
-                      <p className="mt-2 text-sm text-red-500" key={error}>
-                        {error}
-                      </p>
-                    ))}
-                </div> */}
-                <div className="w-full">
-                  <label htmlFor="definition" className="mb-2 block text-sm font-medium text-black">
-                    Definition
-                  </label>
-                  <div className="relative">
-                    <input
-                      id="definition"
-                      name="definition"
-                      type="string"
-                      value={item.definition}
-                      onChange={(event) => handleChange(event, index)}
-                      placeholder="Enter definition"
-                      className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
-                      aria-describedby="definition-error"
-                    />
-                    {/* <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" /> */}
-                  </div>
-                </div>
-                {/* <div id="definition-error" aria-live="polite" aria-atomic="true">
-                  {state.errors?.definition &&
-                    state.errors.definition.map((error: string) => (
-                      <p className="mt-2 text-sm text-red-500" key={error}>
-                        {error}
-                      </p>
-                    ))}
-                </div> */}
-                {pairs.length > 1 && (
-                  <Button onClick={() => handleDeletePair(index)}>Delete</Button>
-                )}
-                {index === pairs.length - 1 && (
-                  <Button onClick={() => handleAddPair()}>Add</Button>
-                )}
+        <div className="mb-1">
+          {pairs.map((item, index) => (
+            <div className="input_container" key={index}>
+              <div className="w-full">
+                <label htmlFor="term" className="mb-2 block text-sm font-medium text-black">
+                  Term
+                </label>
+                <input
+                  id="term"
+                  name="term"
+                  type="string"
+                  value={item.term}
+                  onChange={(event) => handleChange(event, index)}
+                  placeholder="Enter term"
+                  className="peer w-full block rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
+                  aria-describedby="term-error"
+                />
               </div>
-            ))}
-            <p>This is what the data looks like</p>
-            <div className="body"> {JSON.stringify(pairs)} </div>
-          </div>
+              <div className="w-full">
+                <label htmlFor="definition" className="mb-2 block text-sm font-medium text-black">
+                  Definition
+                </label>
+                <input
+                  id="definition"
+                  name="definition"
+                  type="string"
+                  value={item.definition}
+                  onChange={(event) => handleChange(event, index)}
+                  placeholder="Enter definition"
+                  className="peer w-full block rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
+                  aria-describedby="definition-error"
+                />
+              </div>
+              <div className="mb-5">
+              {pairs.length > 1 && (
+                <Button onClick={() => handleDeletePair(index)}>Delete</Button>
+              )}
+              </div>
+              <div className="mb-5">
+              {index === pairs.length - 1 && (
+                <Button onClick={() => handleAddPair()}>Add</Button>
+              )}
+              </div>
+            </div>
+          ))}
+          {/* <p>This is what the data looks like</p>
+          <div className="body"> {JSON.stringify(pairs)} </div> */}
         </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
