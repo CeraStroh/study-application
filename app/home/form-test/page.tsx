809fff9873534a9db5628cf97113f2c29ee1c8b1
CeraStroh <cera.stroh@outlook.com>
2024-02-29 19:05:22 -0600

    simplify form & call action
    - try a different dynamic input method

--- app/home/form-test/page.tsx
+++ app/home/form-test/page.tsx
@@ -2,61 +2,79 @@
 
 import { useState } from "react";
 import { Button } from '@/app/ui/button';
+import { createTest } from "@/app/lib/actions";
 
-export default function AddDynamicInputFields() {
+export default function Form() {
   const [pairs, setPairs] = useState([{ term: "", definition: "" }]);
 
-  const handleAddPair = () => {
-    setPairs([...pairs, { term: "", definition: "" }]);
+  const handleChange = (index: number, event: any) => {
+    let data = [...pairs];
+    data[index][event.target.name] = event.target.value;
+    setPairs(data);
   };
 
-  const handleChange = (event: any, index: number) => {
-    let { name, value } = event.target;
-    console.log(event.target)
-    let onChangeValue = [...pairs];
-    onChangeValue[index][name] = value;
-    console.log(name);
-    setPairs(onChangeValue);
-  };
+  const handleAddPair = () => {
+      let newPair = { term: "", definition: ""};
+      setPairs([...pairs, newPair]);
+    };
 
   const handleDeletePair = (index: number) => {
-    const newArray = [...pairs];
-    newArray.splice(index, 1);
-    setPairs(newArray);
+    let data = [...pairs];
+    data.splice(index, 1);
+    setPairs(data);
   };
 
+  // const submit = (e: any) => {
+  //   e.preventDefault();
+  //   console.log(pairs);
+  // }
 
   return (
-    <div className="container">
-      {pairs.map((item, index) => (
-        <div className="input_container" key={index}>
+    <form action={createTest}>
+      <div className="mb-4">
+        <label htmlFor="title" className="mb-2 block text-sm font-medium text-black">
+          Title
+        </label>
+        <div className="relative">
           <input
-            name="term"
-            type="string"
-            value={item.term}
-            onChange={(event) => handleChange(event, index)}
-            placeholder="Enter term"
-            className="placeholder:text-black text-black"
+              id="title"
+              name="title"
+              type="string"
+              placeholder="Enter title"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
           />
-          <input
-            name="definition"
-            type="string"
-            value={item.definition}
-            onChange={(event) => handleChange(event, index)}
-            placeholder="Enter definition"
-            className="placeholder:text-black text-black"
-          />
-          {pairs.length > 1 && (
-            <Button onClick={() => handleDeletePair(index)}>Delete</Button>
-          )}
-          {index === pairs.length - 1 && (
-            <Button onClick={() => handleAddPair()}>Add</Button>
-          )}
         </div>
-      ))}
+      </div>
+      <div className="container">
+        {pairs.map((input, index) => (
+          <div key={index}>
+            <input
+              name="term"
+              value={input.term}
+              onChange={event => handleChange (index, event)}
+              placeholder="Enter term"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
+            />
+            <input
+              name="definition"
+              value={input.definition}
+              onChange={event => handleChange (index, event)}
+              placeholder="Enter definition"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-black text-black"
+            />
+            {pairs.length > 1 && (
+              <Button onClick={() => handleDeletePair(index)}>Delete</Button>
+            )}
+            {index === pairs.length - 1 && (
+              <Button onClick={handleAddPair}>Add</Button>
+            )}
+          </div>
+        ))}
 
-      <p>This is what the data looks like</p>
-      <div className="body"> {JSON.stringify(pairs)} </div>
-    </div>
+        {/* <p>This is what the data looks like</p>
+        <div className="body"> {JSON.stringify(pairs)} </div> */}
+      </div>
+      <Button type="submit">Create</Button>
+    </form>
   );
 }
