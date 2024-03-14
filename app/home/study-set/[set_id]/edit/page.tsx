0fe94d215a269f320ebd3cdf73a5193e87f6fbad
CeraStroh <cera.stroh@outlook.com>
2024-03-14 11:23:35 -0500

    create dynamic route segment

---
+++ app/home/study-set/[set_id]/edit/page.tsx
@@ -0,0 +1,31 @@
+import Form from '@/app/ui/study-set/edit-form';
+import Breadcrumbs from '@/app/ui/study-set/breadcrumbs';
+import { fetchStudySetBySetId } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+ 
+export default async function Page({ params }: { params: { set_id: string } }) {
+  const set_id = params.set_id;
+  const [studyset] = await Promise.all([
+		fetchStudySetBySetId(set_id)
+	]);
+
+	if (!studyset) {
+		notFound();
+	}
+
+	return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Study Sets', href: '/home/study-set' },
+          {
+            label: 'Edit Study Set',
+            href: `/home/study-set/${set_id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form studyset={studyset} />
+    </main>
+  );
+}
