5614af86f892a15524094efca343c5baf2980072
CeraStroh <cera.stroh@outlook.com>
2024-04-03 15:08:43 -0500

    clean up code, SELECT study_content
    -remove fetchTermsBySetId() & fetchDefinitionsBySetId()

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,7 +2,6 @@ import { sql } from '@vercel/postgres';
 import {
 	StudySetsTable,
 	StudySetForm,
-	StudySetEditForm	
 } from './definitions';
 import { unstable_noStore as noStore } from 'next/cache';
 
@@ -33,7 +32,8 @@ export async function fetchStudySetBySetId(set_id: string) {
 				studysets.set_id,
 				studysets.title,
 				studysets.terms,
-				studysets.definitions
+				studysets.definitions,
+				studysets.study_content
 			FROM studysets
 			WHERE studysets.set_id = ${set_id};
 		`;
@@ -46,39 +46,5 @@ export async function fetchStudySetBySetId(set_id: string) {
 	} catch (error) {
 		console.error('Database Error:', error);
 		throw new Error('Failed to fetch study set.');
-	}
-}
-
-export async function fetchTermsBySetId(set_id: string) {
-	noStore();
-	try {
-		const terms = await sql`
-			SELECT
-				*
-			FROM studysets
-			WHERE set_id = ${set_id};
-		`;
-		console.log(`retrieved terms from ${set_id}: ${terms}`)
-		return terms;
-	} catch (error) {
-		console.error('Database Error:', error);
-		throw new Error('Failed to fetch terms.');
-	}
-}
-
-export async function fetchDefinitionsBySetId(set_id: string) {
-	noStore();
-	try {
-		const definitions = await sql<StudySetEditForm>`
-			SELECT
-				studysets.definitions
-			FROM studysets
-			WHERE studysets.set_id = ${set_id};
-		`;
-
-		return definitions.rows;
-	} catch (error) {
-		console.error('Database Error:', error);
-		throw new Error('Failed to fetch definitions.');
 	}
 }
