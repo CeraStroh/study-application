10639d3a838f5f42bb0f3b7685a1aea65ec82888
CeraStroh <cera.stroh@outlook.com>
2024-04-27 09:37:37 -0500

    remove client side designation accordingly

--- app/home/study-set/[set_id]/flashcards/page.tsx
+++ app/home/study-set/[set_id]/flashcards/page.tsx
@@ -1,60 +1,23 @@
-'use client';
+import { fetchStudySetBySetId } from "@/app/lib/data";
+import { notFound } from "next/navigation";
+import Flashcard from '@/app/ui/study-set/flashcard';
+// import Form from "@/app/ui/study-set/flashcards-form";
 
-import Link from "next/link";
-import { useState } from 'react';
-import Switch from '@mui/material/Switch';
-import { Button } from '@/app/ui/button';
-
-export default function Page({ params }: { params: { set_id: string } }) {
+export default async function Page({ params }: { params: { set_id: string } }) {
 	const set_id = params.set_id;
+	const [studyset] = await Promise.all([
+		fetchStudySetBySetId(set_id)
+	]);
 
-	const [answer, setAnswer] = useState(true);
-  const [score, setScore] = useState(true);
-
-  const handleAnswerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAnswer(event.target.checked);
-  };
-
-  const handleScoreChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setScore(event.target.checked);
-  };
+	if (!studyset) {
+		notFound();
+	}
 
 	return (
 		<main>
-			<form>
-      	<div className="rounded-md bg-gray-400 p-4 md:p-6">
-					<label htmlFor="Answer" className="mb-2 block text-sm font-medium text-black">
-						What do you want to answer with?
-					</label>
-					Definitions
-					<Switch
-					checked={answer}
-					onChange={handleAnswerChange}
-					inputProps={{ 'aria-label': 'controlled' }}
-					/>
-					Terms
-					
-					<label htmlFor="Score" className="mb-2 block text-sm font-medium text-black">
-						Do you want to keep score of correct and incorrect answers?
-					</label>
-					Off
-					<Switch
-					checked={score}
-					onChange={handleScoreChange}
-					inputProps={{ 'aria-label': 'controlled' }}
-					/>
-					On
-				</div>
-				<div className="mt-6 flex justify-end gap-4">
-					<Link
-						href={`/home/study-set/${set_id}/flashcards/practice`}
-						className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
-					>
-						Cancel
-					</Link>
-					<Button type="submit">Start</Button>
-				</div>
-			</form>
+			{/* <Form studyset={studyset} /> */}
+			
+			<Flashcard studyset={studyset} />
 		</main>
 	)
 }
